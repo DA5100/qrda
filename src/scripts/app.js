88df06be60ba721e2deb09daa39070faa8810934
eclipseteamdev-1 <eclipseteamdev1@gmail.com>
2025-07-30 22:53:51 +0700

    Change API

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,53 @@
-let sessionToken = sessionStorage.getItem("session");
+const firebaseConfig = {
+    apiKey: "AIzaSyCBeFJtPKEMURY-iUDUR4I6gWKjmlTk_3E",
+    authDomain: "authdramaarena.firebaseapp.com",     
+    databaseURL: "https://authdramaarena.firebaseio.com",
+    projectId: "authdramaarena",                      
+    storageBucket: "authdramaarena.appspot.com",      
+    messagingSenderId: "348583435302",             
+    appId: "1:348583435302:web:someUniqueWebId",    
+    measurementId: "G-DGF0CP099H"                  
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 const params = new URLSearchParams(window.location.search);
 const userToken = params.get("session");
+const serialKey = params.get("serial_key");
 
 document.addEventListener("DOMContentLoaded", function(){
-    if(!userToken){
-        alert("Token tidak ada!")
+    auth.onAuthStateChanged((user) => {
+    if (!user) {
+        alert("Anda belum login. Silakan login terlebih dahulu.");
+        window.location.href = "https://da5100.github.io/auth/";
+        return;
+    }
+    if (user) {
+        console.log("User is logged in:", user.displayName); 
+    
+    const email = String(user.email);
+    const keyRef = db.collection("lisensi").doc(serialKey);
+    const emailmd5 = CryptoJS.MD5(email).toString();
+
+    keyRef.get().then((doc) => {
+    let getData = doc.data();
+    const emailDatamd5 = CryptoJS.MD5(getData.email).toString();
+
+    if(!userToken || !doc.exists) {
+        alert("Token/Serial tidak ada!")
         window.location.href = "https://da5100.github.io/auth/"
-    } else if(!sessionToken){
-        alert("Tidak ada token yang terdaftar")
+    } else if(!serialKey){
+        alert("Serial key tidak ada!")
         window.location.href = "https://da5100.github.io/auth/"
-    }else if(userToken !== sessionToken){
-        alert("Token tidak valid! Silahkan login kembali!")
+    }else if(emailDatamd5 !== emailmd5){
+        alert("Sesi tidak sah! Coba login kembali menggunakan akun yang terdaftar!")
         window.location.href = "https://da5100.github.io/auth/"
     } else {
-        console.log("Token " + userToken + "valid!")
+        console.log("Email: " + getData.email + " valid!, md5: " + emailDatamd5 + " | " + emailmd5);
     }
+    });
+    }
+    
+    
+});
 })
