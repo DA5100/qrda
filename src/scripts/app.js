4953ea45f8fe3f555efa7f6769fe6ee759d0acab
eclipseteamdev-1 <eclipseteamdev1@gmail.com>
2025-07-30 16:27:42 +0700

    ++token validator

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,18 @@
-let sessionToken = sessionStorage.session();
-const params = new document.S
+let sessionToken = sessionStorage.getItem("session").toString();
+const params = new URLSearchParams(window.location.search);
+const userToken = params.get("session");
+
+document.addEventListener("DOMContentLoaded", function(){
+    if(!userToken){
+        alert("Token tidak ada!")
+        window.location.href = "https://da5100.github.io/auth/"
+    } else if(!sessionToken){
+        alert("Tidak ada token yang terdaftar")
+        window.location.href = "https://da5100.github.io/auth/"
+    }else if(userToken !== sessionToken){
+        alert("Token tidak valid! Silahkan login kembali!")
+        window.location.href = "https://da5100.github.io/auth/"
+    } else {
+        console.log("Token " + userToken + "valid!")
+    }
+})
